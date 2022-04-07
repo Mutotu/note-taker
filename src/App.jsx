41390fd1b1f3a563b708726823b357b8f2f20385
mutotu <muttalip.aydemir@gmail.com>
2022-04-07 23:23:14 +0100

    created form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default () => <div>Hello World!</div>
+export default () => {
+  <div>Hello World!</div>;
+  const [input, setInput] = useState("");
+  const [notes, setNotes] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNotes([{ body: input }, ...notes]);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          placeholder='add note'
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+        <input type='submit' value='Add' />
+      </form>
+    </div>
+  );
+};
